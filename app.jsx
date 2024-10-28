fea005cb2068b1fd4f072f0c171cdf49bce9b0a7
Gemmacr <146110914+Gemmacr@users.noreply.github.com>
2024-10-27 20:46:35 -0400

    Update app.jsx
    Added a button to go to the weather website

--- app.jsx
+++ app.jsx
@@ -1,20 +1,24 @@
 import Note from './components/Note'
-
+ 
 const App = ({ notes }) => {
+  const handleButtonClick = () => {
+    window.open("https://openweathermap.org/city/6094817", "_blank");
+  }
+ 
   return (
-    <div>
-      <h1>Rain or Stain</h1>
-      <button>
-      Determine if you Can Stain
-    </button>
-      <ul>
+<div>
+<h1>Rain or Stain</h1>
+<button onClick={handleButtonClick}>
+        Weather App
+</button>
+<ul>
         {notes.map(note => 
-          <Note key={note.id} note={note} />
+<Note key={note.id} note={note} />
         )}
-      </ul>
-    </div>
+        <button>Determine if You can Stain</button>
+</ul>
+</div>
   )
 }
-
-
+ 
 export default App
